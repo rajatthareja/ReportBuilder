8b42bf165346ff81f8aefc29ffb4e270a6a91da3
Rajat Thareja <mail@rajatthareja.com>
2016-02-18 18:11:55 +0530

    param update

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ gem install report_builder
     
     # @param [Object] file_or_dir Input json file, Default nil (current directory),  array of json files or path, json files path
     # @param [String] output_file_name Output file name, Default test_report
-    # @param [String] output_file_type Output file type, Default [:html], Options [:json] and [:json, :html] or ['html', 'json']
+    # @param [Array] output_file_type Output file type, Default [:html], Options [:json] and [:json, :html] or ['html', 'json']
     # @param [Array] tabs Tabs to build, by default [:overview, :features, :errors], Options [:overview, :features, :scenarios, :errors] or ['overview', 'features', 'scenarios', 'errors']
 
     # 1 
--- lib/report_builder.rb
+++ lib/report_builder.rb
@@ -62,7 +62,7 @@ class ReportBuilder
 
 # @param [Object] file_or_dir Input json file, Default nil (current directory),  array of json files or path, json files path
 # @param [String] output_file_name Output file name, Default test_report
-# @param [String] output_file_type Output file type, Default [:html], Options [:json] and [:json, :html] or ['html', 'json']
+# @param [Array] output_file_type Output file type, Default [:html], Options [:json] and [:json, :html] or ['html', 'json']
 # @param [Array] tabs Tabs to build, by default [:overview, :features, :errors], Options [:overview, :features, :scenarios, :errors] or ['overview', 'features', 'scenarios', 'errors']
   def self.build_report(file_or_dir = nil, output_file_name = 'test_report', output_file_type = [:html], tabs = [:overview, :features, :errors])
 
