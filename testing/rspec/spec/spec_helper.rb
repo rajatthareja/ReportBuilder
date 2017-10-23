5415315666d6766529f71a633d6ffd8e5e492bf2
Eric Kessler <morrow748@gmail.com>
2017-10-23 00:42:57 -0400

    Windows CI (#56)
    * Fix cross platform encoding issue
    
    Forcing UTF-8 encoding during testing so that we get the same results
    regardless of the operating system the tests are running on.
    
    * Enable AppVeyor for the project
    
    Added the needed config file that will activate AppVeyor builds.

--- testing/rspec/spec/spec_helper.rb
+++ testing/rspec/spec/spec_helper.rb
@@ -8,6 +8,9 @@ require_relative "#{here}/../../file_helper"
 
 RSpec.configure do |config|
 
+  # Ensure that we read in our test files the same on all platforms
+  Encoding.default_external = 'UTF-8'
+
   config.after(:all) do
     ReportBuilder::FileHelper.clear_created_directories
   end
